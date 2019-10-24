5e275b9b6ff22e725ceb96f3a590877b713a6527
Bert Keller <bertwinkeller@gmail.com>
2019-10-24 12:15:08 -0700

    bring in moment and axios

--- liri.js
+++ liri.js
@@ -7,13 +7,33 @@ require("dotenv").config();
 var keys = require("./keys.js");
 
 // spotify key with your information to run requests
+var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 
+
+// sets process arguments for command line reference
 let command = process.argv[2]
+let title = process.argv[3]
+
+// brings in axios 
+var axios = require('axios');
+
+var moment = require('moment');
 
 switch(command){
-    case "concert-this":
 
+    case "concert-this":
+    let artist = title;
+    let url = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+    axios.get(url)
+    .then(function (response) {
+        // handle success
+        console.log(response);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
 
     break;
     case "spotify-this-song":
