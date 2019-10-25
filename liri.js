9c34efe26167eb1af1edef71d059c8976d8e91c2
Bert Keller <bertwinkeller@gmail.com>
2019-10-24 18:34:04 -0700

    spotify app partially working

--- liri.js
+++ liri.js
@@ -1,8 +1,6 @@
 // Reads environment variables in dotenv package
 require("dotenv").config();
 
-
-
 // imports keys.js file and stores it in a variable 
 var keys = require("./keys.js");
 
@@ -18,33 +16,48 @@ let title = process.argv[3]
 // brings in axios 
 var axios = require('axios');
 
+// reference moment
 var moment = require('moment');
+moment().format();
 
-switch(command){
+switch (command) {
 
     case "concert-this":
-    let artist = title;
-    let url = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
-    axios.get(url)
-    .then(function (response) {
-        // handle success
-        console.log(response);
-      })
-      .catch(function (error) {
-        // handle error
-        console.log(error);
-      })
+        let artist = title;
+        let url = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+        axios.get(url)
+            .then(function (response) {
+                // handle success
+                console.log(response);
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            })
 
-    break;
+        break;
     case "spotify-this-song":
+        if (!title) {
+            song = 'The Sign';
+        } else {
+            song = title;
+        }
+        spotify.search({ type: 'track', query: song }, function (err, data) {
+            if (err) {
+
+                return console.log('Error occurred: ' + err);
+            }
+
+            console.log(data.tracks.items[0]);
+        });
 
 
-    break;
+break;
     case "move-this":
 
-    break;
+break;
     case "do-what-it-says":
 
 
-    break;
+break;
 }
